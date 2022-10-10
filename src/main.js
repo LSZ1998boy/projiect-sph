3dd87eaaf65ef7b565fc6da7c807247f90162d00
liusanzhu <liusanzhu1998@163.com>
2022-10-10 10:11:15 +0800

    “创建非路由组件简单使用路由组件但还没配置”

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入路由
+import router from '@/router'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 注册路由
+  // 注册路由信息 组件身上带有 @route  @router 属性
+  // 不管是否是路由组件
+  router
 }).$mount('#app')
